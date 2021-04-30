aca0cb86d879f6c23344fd78f052072e337a9f99
Aleksey.Grokhotov <lukomorie2001@gmail.com>
2021-04-30 12:16:38 +0300

    Распаковка с помощью ZipFile

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,32 +1,38 @@
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
-import java.io.IOException;
+import sun.misc.IOUtils;
+
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 import java.util.zip.ZipInputStream;
 
 public class MainApp {
     public static void main(String[] args) {
-        readAndSwap("../zipfile/zipka.zip");
+        readAndSwap("zipka.zip");
     }
 
     private static void readAndSwap(String path) {
         try {
-            FileInputStream fileInputStream = new FileInputStream(path);
-            BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream);
-            ZipInputStream zipInputStream = new ZipInputStream(bufferedInputStream);
 
-            ZipEntry zipEntry;
-            List<String> contents = new ArrayList<>();
+            ZipFile zipFile = new ZipFile(path);
+            ZipEntry zipEntry = zipFile.getEntry("testfile.txt");
+            InputStream inputStream = zipFile.getInputStream(zipEntry);
 
-            while ((zipEntry = zipInputStream.getNextEntry()) != null) {
-                contents.add(zipEntry.toString());
-            }
 
-            System.out.println(contents);
+//            while ((zipEntry = zipInputStream.getNextEntry()) != null) {
+////                contents.add(zipEntry.toString());
+//            }
+
+            String contents = new BufferedReader(new InputStreamReader(inputStream))
+                    .lines().peek(System.out::println).collect(Collectors.joining("\n"));
+
+            contents = swap(contents);
+
+
         } catch (Exception e) {
             e.printStackTrace();
         }
