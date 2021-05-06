1273191b5fcddb2cb29a6d045382ebb59f3ac5bc
Aleksey.Grokhotov <lukomorie2001@gmail.com>
2021-05-06 15:21:28 +0300

    починить абсолютный путь: разобраться с \ в URI

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -25,8 +25,8 @@ public class MainApp {
 //            System.out.println(zip.getAbsolutePath());
 //        } catch (URISyntaxException e) {e.printStackTrace();}
 
-        File file = new File("resources/zipka.zip");
-        System.out.println(file.getAbsolutePath());
+//        File file = new File("resources/zipka.zip");
+//        System.out.println(file.getAbsolutePath());
 
     }
 
@@ -42,16 +42,14 @@ public class MainApp {
         Path pathForFS = Paths.get(file.getParent(), filler.getPathToZip());
          */
 
-
-
-
-
         try {
             MainApp app = new MainApp();
             URL resource = app.getClass().getClassLoader().getResource(filler.getPathToZip());
             File zipFile = Paths.get(resource.toURI()).toFile();
+//            System.out.println(zipFile.getAbsolutePath());
 
             FileInputStream fileInputStream = new FileInputStream(zipFile.getAbsolutePath());
+//            FileInputStream fileInputStream = new FileInputStream(filler.getPathToZip());
              BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream);
              ZipInputStream zipInputStream = new ZipInputStream(bufferedInputStream);
 
@@ -64,7 +62,7 @@ public class MainApp {
                 }
             }
 
-            String alteredContents = contents.replace("Java", "Avaj");
+            String alteredContents = contents.replace(filler.getWordBefore(), filler.getWordAfter());
             createTempFile(alteredContents);
             addToArchive(filler);
 //            deleteTempFile();
@@ -82,8 +80,12 @@ public class MainApp {
      * @return Содержимое файла в архиве
      * @throws IOException
      */
-    private static String getContents(String pathToZip, String fileNameInZip) throws IOException {
-        ZipFile zipFile = new ZipFile(pathToZip);
+    private static String getContents(String pathToZip, String fileNameInZip) throws IOException, URISyntaxException {
+
+        URL resource = new MainApp().getClass().getClassLoader().getResource(pathToZip);
+        File file = Paths.get(resource.toURI()).toFile();
+
+        ZipFile zipFile = new ZipFile(file.getAbsolutePath());
         ZipEntry zipEntry = zipFile.getEntry(fileNameInZip);
         InputStream inputStream = zipFile.getInputStream(zipEntry);
 
@@ -112,19 +114,23 @@ public class MainApp {
      *
      * @throws IOException
      */
-    private static void addToArchive(Filler filler) throws IOException {
+    private static void addToArchive(Filler filler) throws IOException, URISyntaxException {
 
         Map<String, String> env = new HashMap<>();
         env.put("create", "true");
 
 //        @todo: поправить абсолютный путь
-        File file = new File(filler.getPathToZip());
+//        File file = new File(filler.getPathToZip());
         File altered = new File("altered.tmp");
-        Path pathForFS = Paths.get(file.getParent(), filler.getPathToZip());
-//        URI uri = URI.create("jar:file:/Users/alexey/IdeaProjects/zipfile/zipka.zip");
+//        Path pathForFS = Paths.get(file.getParent(), filler.getPathToZip());
 
 
-        try (FileSystem zipfs = FileSystems.newFileSystem(pathForFS, ClassLoader.getSystemClassLoader())) {
+        URL resource = new MainApp().getClass().getClassLoader().getResource(filler.getPathToZip());
+        File zipFile = Paths.get(resource.toURI()).toFile();
+//        URI uri = URI.create("jar:file:" + zipFile.getAbsolutePath());
+        URI uri = zipFile.toURI();
+
+        try (FileSystem zipfs = FileSystems.newFileSystem(uri, env)) {
 //            Path externalTxtFile = Paths.get("/Users/alexey/IdeaProjects/zipfile/altered.tmp");
             Path externalTxtFile = Paths.get(altered.getAbsolutePath());
             Path pathInZipfile = zipfs.getPath(filler.getFileNameInZip());
