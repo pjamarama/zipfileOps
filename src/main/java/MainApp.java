096455df4f36e9d0cb6133f9be2a8f7c7f1d3c81
Aleksey.Grokhotov <lukomorie2001@gmail.com>
2021-05-11 17:10:07 +0300

    разобраться с NPE в getAbsPath

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -1,10 +1,8 @@
 import java.io.*;
-import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.file.FileSystem;
 import java.nio.file.*;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
@@ -13,20 +11,10 @@ import java.util.zip.ZipInputStream;
 // @todo: избавиться от временного файла
 public class MainApp {
     public static void main(String[] args) {
+
         readAndSwap(new Filler(
-                "Java", "Kotlin", "zipka.zip", "testfile.txt"
+                "Java", "Kotlin", "zibberish.zip", "testfile.txt"
         ));
-
-//        Filler f = new  Filler("Java", "Avaj", "zipka.zip", "testfile.txt");
-//        MainApp app = new MainApp();
-
-//        try {
-//            File zip = app.getFileFromResource("zipka.zip");
-//            System.out.println(zip.getAbsolutePath());
-//        } catch (URISyntaxException e) {e.printStackTrace();}
-
-//        File file = new File("resources/zipka.zip");
-//        System.out.println(file.getAbsolutePath());
 
     }
 
@@ -37,21 +25,10 @@ public class MainApp {
      */
     private static void readAndSwap(Filler filler) {
 
-        /*
-        File file = new File(filler.getPathToZip());
-        Path pathForFS = Paths.get(file.getParent(), filler.getPathToZip());
-         */
-
         try {
-            MainApp app = new MainApp();
-            URL resource = app.getClass().getClassLoader().getResource(filler.getPathToZip());
-            File zipFile = Paths.get(resource.toURI()).toFile();
-//            System.out.println(zipFile.getAbsolutePath());
-
-            FileInputStream fileInputStream = new FileInputStream(zipFile.getAbsolutePath());
-//            FileInputStream fileInputStream = new FileInputStream(filler.getPathToZip());
-             BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream);
-             ZipInputStream zipInputStream = new ZipInputStream(bufferedInputStream);
+            FileInputStream fileInputStream = new FileInputStream(getAbsPath(filler.getPathToZip()));
+            BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream);
+            ZipInputStream zipInputStream = new ZipInputStream(bufferedInputStream);
 
             ZipEntry entry;
             String contents = "";
@@ -65,7 +42,6 @@ public class MainApp {
             String alteredContents = contents.replace(filler.getWordBefore(), filler.getWordAfter());
             createTempFile(alteredContents);
             addToArchive(filler);
-//            deleteTempFile();
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -80,12 +56,9 @@ public class MainApp {
      * @return Содержимое файла в архиве
      * @throws IOException
      */
-    private static String getContents(String pathToZip, String fileNameInZip) throws IOException, URISyntaxException {
+    private static String getContents(String pathToZip, String fileNameInZip) throws IOException {
 
-        URL resource = new MainApp().getClass().getClassLoader().getResource(pathToZip);
-        File file = Paths.get(resource.toURI()).toFile();
-
-        ZipFile zipFile = new ZipFile(file.getAbsolutePath());
+        ZipFile zipFile = new ZipFile(getAbsPath(pathToZip));
         ZipEntry zipEntry = zipFile.getEntry(fileNameInZip);
         InputStream inputStream = zipFile.getInputStream(zipEntry);
 
@@ -114,51 +87,30 @@ public class MainApp {
      *
      * @throws IOException
      */
-    private static void addToArchive(Filler filler) throws IOException, URISyntaxException {
-
-        Map<String, String> env = new HashMap<>();
-        env.put("create", "true");
-
-//        @todo: поправить абсолютный путь
-//        File file = new File(filler.getPathToZip());
+    private static void addToArchive(Filler filler) throws IOException {
         File altered = new File("altered.tmp");
-//        Path pathForFS = Paths.get(file.getParent(), filler.getPathToZip());
+        Path absPathToZip = Paths.get(getAbsPath(filler.getPathToZip()));
 
-
-        URL resource = new MainApp().getClass().getClassLoader().getResource(filler.getPathToZip());
-        File zipFile = Paths.get(resource.toURI()).toFile();
-//        URI uri = URI.create("jar:file:" + zipFile.getAbsolutePath());
-        URI uri = zipFile.toURI();
-
-        try (FileSystem zipfs = FileSystems.newFileSystem(uri, env)) {
-//            Path externalTxtFile = Paths.get("/Users/alexey/IdeaProjects/zipfile/altered.tmp");
+        try (FileSystem zipfs = FileSystems.newFileSystem(absPathToZip, null)) {
             Path externalTxtFile = Paths.get(altered.getAbsolutePath());
             Path pathInZipfile = zipfs.getPath(filler.getFileNameInZip());
             Files.copy(externalTxtFile, pathInZipfile, StandardCopyOption.REPLACE_EXISTING);
         }
     }
 
-//    /**
-//     * Удаляет временный файл "altered.tmp".
-//     *
-//     * @throws IOException
-//     */
-//    private static void deleteTempFile() throws IOException {
-//
-//        Path tempFile = Paths.get("/Users/alexey/IdeaProjects/zipfile/altered.tmp");
-//        Files.delete(tempFile);
-//
-//    }
+    /**
+     * Возвращает строковый объект, содержащий абсолютный путь файла, имя которого передаётся в параметах
+     *
+     * @param fileName Имя файла
+     * @return Абсолютный путь файла
+     */
+    private static String getAbsPath(String fileName) {
 
-//    private File getFileFromResource(String fileName) throws URISyntaxException {
-//
-//        ClassLoader classLoader = getClass().getClassLoader();
-//        URL url = classLoader.getResource(fileName);
-//        if (url == null) {
-//            throw new IllegalArgumentException("File not found: " + fileName);
-//        } else {
-//            return new File(url.toURI());
-//        }
-//
-//    }
+        ClassLoader classLoader = MainApp.class.getClassLoader();
+        URL s = classLoader.getResource(fileName);
+        File file = new File(classLoader.getResource(fileName).getFile());
+        System.out.println(file.getAbsolutePath());
+        return file.getAbsolutePath();
+
+    }
 }
